49bb09e4224c3f4a52388fd3a6cc0576367ad0ba
fengjianjian <fengjj@7zhilu.com>
2018-12-24 14:41:55 +0800

    通过注解实现配置

--- demo_consumer/src/main/java/com/sevenlu/dubbo/demo_consumer/controller/ConsumerController.java
+++ demo_consumer/src/main/java/com/sevenlu/dubbo/demo_consumer/controller/ConsumerController.java
@@ -3,7 +3,6 @@ package com.sevenlu.dubbo.demo_consumer.controller;
 import com.alibaba.dubbo.config.annotation.Reference;
 import com.sevenlu.dubbo.api.DemoService;
 import com.sevenlu.dubbo.model.User;
-import org.springframework.stereotype.Component;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -14,8 +13,6 @@ public class ConsumerController {
     @Reference(
             interfaceClass = DemoService.class,
             version = "${demo.service.version}",
-            application = "${dubbo.application.id}",
-            registry = "${dubbo.registry.address}",
             loadbalance = "roundrobin")
     private DemoService demoService;
 
